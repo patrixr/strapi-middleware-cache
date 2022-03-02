4cf0f28afb1b1f01ebcda6f06042869191212338
Sacha STAFYNIAK <sacha@digisquad.io>
2022-03-02 17:13:38 +0100

    chore: use strapi.db.lifecycles to subscribe to role changes

--- packages/strapi-plugin-rest-cache/server/register.js
+++ packages/strapi-plugin-rest-cache/server/register.js
@@ -8,40 +8,16 @@
  * @param {{ strapi: Strapi }} strapi
  */
 module.exports = ({ strapi }) => {
-  // @todo: use cache provider instead of hard-coded LRU
   const store = strapi.plugin('rest-cache').service('cacheStore');
 
   // watch for changes in any roles -> clear all cache
   // need to be done before lifecycles are registered
-  // @todo: can we use strapi.eventHub instead of lifecycles?
   if (strapi.plugin('users-permissions')) {
-    const role = strapi.plugin('users-permissions').contentType('role');
-
-    if (!role.lifecycles) {
-      role.lifecycles = {
-        async afterUpdate() {
-          strapi.log.info('A role has been updated, invalidating cache...');
-          await store.reset();
-        },
-      };
-    } else if (role.lifecycles.afterUpdate) {
-      const originalAfterUpdate = role.lifecycles.afterUpdate;
-      role.lifecycles = {
-        ...role.lifecycles,
-        async afterUpdate(event) {
-          strapi.log.info('A role has been updated, invalidating cache...');
-          await originalAfterUpdate(event);
-          await store.reset();
-        },
-      };
-    } else {
-      role.lifecycles = {
-        ...role.lifecycles,
-        async afterUpdate() {
-          strapi.log.info('A role has been updated, invalidating cache...');
-          await store.reset();
-        },
-      };
-    }
+    strapi.db.lifecycles.subscribe({
+      models: ['plugin::users-permissions.role'],
+      async beforeDelete() {
+        await store.reset();
+      },
+    });
   }
 };
