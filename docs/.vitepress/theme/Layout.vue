3aca56acbe644b4a086aa5be0fbc1c7e5ccfb95e
Sacha STAFYNIAK <sacha@digisquad.io>
2023-01-01 04:03:27 +0100

    feat(doc): add giscus comments

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,35 @@
+<script setup>
+import Giscus from '@giscus/vue'
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #doc-after>
+      <div class="comments">
+        <Giscus
+          repo="strapi-community/strapi-plugin-rest-cache"
+          repo-id="R_kgDOGtExJQ"
+          category="Documentation"
+          category-id="DIC_kwDOGtExJc4CTWPj"
+          mapping="pathname"
+          strict="0"
+          reactions-enabled="1"
+          emit-metadata="0"
+          input-position="bottom"
+          theme="light"
+          lang="en"
+          loading="lazy"
+        />
+      </div>
+    </template>
+  </Layout>
+</template>
+
+<style scoped>
+.comments {
+  margin-top: 2rem;
+}
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,10 @@
 import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  // override the Layout with a wrapper component that
+  // injects the slots
+  Layout
+}
